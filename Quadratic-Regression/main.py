0df41d0bf6fb42e00fb2b41e1d287059e36196c7
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2025-03-27 13:59:14 -0500

    implement three protocols for quadratic dataset

--- Quadratic-Regression/main.py
+++ Quadratic-Regression/main.py
@@ -1,10 +1,22 @@
 import torch
 import copy
+import itertools
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+from scipy.optimize import curve_fit
 
-y_threshold = 0.5
+## Basic configurations
+n = 2                   # number of clients
+num_of_labels = 2       # number of labels of the classification task
+y_threshold = 0.5       # threshold for the label
+s_eps = 50              # each client add s_eps more samples to estimate the gradient of shapley share
+s_lr = 1000             # learning rate for the share update
+costs = np.random.uniform(0, 0.001, n)
+training_size=1000      # number of training samples
+fit_sample_delta=100    # delta value for fitting the accuracy function
+testing_size=100        # number of testing samples
+cost_scalar_beta=0.8    # beta value for br-bg method
 
 def generate_random_quadratic_data(A, B, c, num_points=100, delta=0.5, label=1):
     # X = np.random.uniform(-10, 10, (num_points, 10))
@@ -61,15 +73,15 @@ class Client():
         self.train_X, self.train_y = generate_random_quadratic_data(A, B, c, num_points=training_size, delta=1.0, label=label)
         self.test_X, self.test_y = generate_random_quadratic_data(A, B, c, num_points=testing_size, delta=1.0, label=label)
 
-    def train(self, global_model, epochs=10, lr=0.005):
+    def train(self, global_model, epochs=10, lr=0.005, share_size=10):
         local_model = copy.deepcopy(global_model)
         optimizer = optim.SGD(local_model.parameters(), lr=lr)
         loss_fn = nn.BCELoss()
 
         for epoch in range(epochs):
             optimizer.zero_grad()
-            y_pred = local_model(self.train_X)
-            loss = loss_fn(y_pred, self.train_y)
+            y_pred = local_model(self.train_X[:share_size, :])
+            loss = loss_fn(y_pred, self.train_y[:share_size])
             loss.backward()
             optimizer.step()
         
@@ -83,35 +95,202 @@ class Client():
             total = self.test_y.size(0)
             return correct / total
 
-def fl_run(num_rounds=300, local_epochs=5, lr=0.001):
-    A = np.random.randn(10, 10)
-    A = (A + A.T) / 2 
-    B = np.random.randn(10)
-    c = np.random.randn()
+class GroundTruth():
+    def __init__(self):
+        self.A = np.random.randn(10, 10)
+        self.A = (self.A + self.A.T) / 2 
+        self.B = np.random.randn(10)
+        self.c = np.random.randn()
+    
+    def parameters(self):
+        return self.A, self.B, self.c
 
-    client1 = Client(A, B, c, training_size=1000, testing_size=100, label=1)
-    client2 = Client(A, B, c, training_size=1000, testing_size=100, label=0)
-
+## Protocol 0: FL with fixed shares
+def fl_run(clients, s_vec, num_rounds=300, local_epochs=5, lr=0.001, verbose=False):
     global_model = QuadraticClassifier()
-    print("Initial model parameters:")
-
     for round in range(num_rounds):
-        params_a, loss_a = client1.train(global_model, epochs=local_epochs, lr=lr) 
-        params_b, loss_b = client2.train(global_model, epochs=local_epochs, lr=lr)         
-
+        params_all = []
+        for i, client in enumerate(clients):
+            params, _ = client.train(global_model, epochs=local_epochs, lr=lr, share_size=s_vec[i])
+            params_all.append(params.values())
+        
         with torch.no_grad():
-            for param_g, param_a, param_b in zip(global_model.parameters(), params_a.values(), params_b.values()):
-                param_g.copy_((param_a + param_b) / 2)
+            for param_g, *params in zip(global_model.parameters(), *params_all):
+                param_g.copy_(sum(params) / n)
 
         if round % 10 == 0:
-            acc_a = client1.compute_accuracy(global_model)
-            acc_b = client2.compute_accuracy(global_model)
-            print(f"Round {round}: Accuracy A = {acc_a:.2%}, Accuracy B = {acc_b:.2%}")
+            accs = []
+            for client in clients:
+                acc = client.compute_accuracy(global_model)
+                accs.append(acc)
+            if verbose:
+                print(f"Round {round}: Accuracy {accs}, Shares {s_vec}")
 
-    return global_model
+    return global_model, accs
+
+## Protocol 1: FL without closed form
+def fl_run_without_closed_form(clients: list[Client], num_rounds=300, local_epochs=5, lr=0.001, verbose=False):
+    # initial data shares 
+    s_vec = [10 for _ in range(n)]
+    assert(all([s > 0 for s in s_vec]))
+    
+    global_model = QuadraticClassifier()
+    
+    for round in range(num_rounds):
+        params_all = []
+        params_eps_all = []
+
+        for i, client in enumerate(clients):
+            params, _ = client.train(global_model, epochs=local_epochs, lr=lr, share_size=s_vec[i])
+            params_all.append(params.values())
+            params_eps, _ = client.train(global_model, epochs=local_epochs, lr=lr, share_size=s_vec[i] + s_eps)
+            params_eps_all.append(params_eps.values())
+        
+        all_permutations = list(itertools.permutations(range(n)))
+        gradients = np.zeros(n)
+        for perm in all_permutations:
+            for i in range(n):
+                # get model T_i
+                model_fst_i = copy.deepcopy(global_model)
+                with torch.no_grad():
+                    for param_g, *param_i in zip(model_fst_i.parameters(), *params_all):
+                        # print(sum(param_i[:i+1]))
+                        param_g.copy_(sum([param_i[perm[j]] for j in range(i)]) / (i + 1))
+                # get model T_i^eps
+                model_eps_fst_i = copy.deepcopy(global_model)
+                with torch.no_grad():
+                    for param_g, *param_i in zip(model_eps_fst_i.parameters(), *params_all, *params_eps_all):
+                        param_g.copy_(((sum([param_i[perm[j]] for j in range(i-1)])) + param_i[n + perm[i]]) / (i + 1))
+                
+                ## estimate the gradient
+                accs, accs_eps = [], []
+                for client in clients:
+                    acc = client.compute_accuracy(model_fst_i)
+                    acc_eps = client.compute_accuracy(model_eps_fst_i)
+                    accs.append(acc)
+                    accs_eps.append(acc_eps)
+                gradients[perm[i]] += (sum(accs_eps) - sum(accs)) / s_eps
+                
+        gradients /= len(all_permutations)
+        for i, s, g in zip(list(range(n)), s_vec, gradients):
+            s_updated = int(s + s_lr * (g - costs[i]))
+            if s_updated >= training_size or s_updated <= 0:
+                continue
+            else:
+                s_vec[i] = s_updated
+
+        if verbose:
+            print(gradients, s_vec)
+        
+        ## Update the global model
+        with torch.no_grad():
+            for param_g, *params in zip(global_model.parameters(), *params_all):
+                param_g.copy_(sum(params) / n)
+        
+        if round % 10 == 0:
+            accs = []
+            for client in clients:
+                acc = client.compute_accuracy(global_model)
+                accs.append(acc)
+            if verbose:
+                print(f"Round {round}: Accuracy {accs}, Shares {s_vec}")
+
+    return global_model, accs, s_vec
+
+# Protcol 2: FL with closed form
+def accuracy_func(S, *w):
+    len_of_S = len(S)
+    return 1 - 1 / (1 + sum([w[i] * S[i] for i in range(len_of_S)]))
+
+def derivate_of_accuracy(S, *w, j):
+    len_of_S = len(S)
+    return w[j] / (1 + sum([w[i] * S[i] for i in range(len_of_S)]))**2
+
+def fit_closed_form_accuracy(clients: list[Client], num_rounds=300, local_epochs=5, lr=0.001, verbose=False):
+    print("Begin fitting the closed form accuracy function!")
+    num_of_samples_per_dimension = training_size // fit_sample_delta
+    s_vecs = list(itertools.product(range(num_of_samples_per_dimension + 1), repeat=n))
+    all_accs = np.zeros((len(s_vecs), n))
+
+    for s_idx, s_vec in enumerate(s_vecs):
+        _, accs = fl_run(clients, s_vec, num_rounds=num_rounds // 10, local_epochs=local_epochs, lr=lr, verbose=False)
+        all_accs[s_idx] = accs
+    if verbose:
+        print(all_accs)
+
+    w = np.zeros((n, n))
+    for i in range(n):
+        try:
+            popt, _ = curve_fit(accuracy_func, 
+                                [[s_vecs[j][i] for j in range(len(s_vecs))] for i in range(n)],
+                                all_accs[:, i], np.zeros(n))
+            w[i] = popt
+            if verbose:
+                print("popt:", popt)
+        except:
+            print("Fail to fit curve")
+    print("Finish fitting the closed-form accuracy function!", w)
+
+    return w
+
+def fl_run_with_closed_form(clients: list[Client], w, method:str, num_rounds=300, local_epochs=5, lr=0.001, verbose=False):
+    global_model = QuadraticClassifier()
+    s_vec = [10 for _ in range(n)]
+
+    def update_share(S, w, i):
+        derivate = derivate_of_accuracy(S, *w, j=i)
+        if method == "br":
+            s_updated = int(S[i] + s_lr * (derivate - costs[i]))
+        elif method == "br-bg": 
+            s_updated = int(S[i] + s_lr * (derivate - (1 - cost_scalar_beta) * costs[i]))
+        if s_updated >= training_size or s_updated <= 0:
+            return S[i]
+        else:
+            return s_updated
+
+    for round in range(num_rounds):
+        params_all = []
+        for i, client in enumerate(clients):
+            params, _ = client.train(global_model, epochs=local_epochs, lr=lr, share_size=s_vec[i])
+            params_all.append(params.values())
+            s_vec[i] = update_share(s_vec, w[i], i)
+
+        with torch.no_grad():
+            for param_g, *params in zip(global_model.parameters(), *params_all):
+                param_g.copy_(sum(params) / n)
+
+        if round % 10 == 0:
+            accs = []
+            for client in clients:
+                acc = client.compute_accuracy(global_model)
+                accs.append(acc)
+            if verbose:
+                print(f"Round {round}: Accuracy {accs}, Shares {s_vec}")
+
+    return global_model, accs, s_vec
 
 if __name__ == "__main__":
-    global_model = fl_run()
-    print("Parameters of the global model:")
-    for name, param in global_model.named_parameters():
-        print(f"{name}: {param.data.numpy()}")
+    g = GroundTruth()
+    A, B, c = g.parameters()
+    clients = []
+    for i in range(num_of_labels):
+        client = Client(A, B, c, training_size=training_size, testing_size=testing_size, label=i)
+        clients.append(client)
+
+    ## Run the FL protocol without closed form
+    global_model, accs, s_vec = fl_run_without_closed_form(clients)
+    cost_of_clients = [costs[i] * s_vec[i] for i in range(n)]
+    print("Accuracy of Fed-Shap: ", accs)
+    print("Shares of Fed-Shap  :", s_vec)
+    print("Costs  of Fed-Shap  :", cost_of_clients)
+    print("Welfare of Fed-Shap :", [acc - cost for acc, cost in zip(accs, cost_of_clients)])    
+
+    ## Run the FL protocol with closed form
+    w = fit_closed_form_accuracy(clients)     # find the fitted closed form accuracys
+    for method in ["br", "br-bg"]:
+        global_model, accs, s_vec = fl_run_with_closed_form(clients, w, method)
+        cost_of_clients = [costs[i] * s_vec[i] for i in range(n)]
+        print("Accuracy of  {}: {}".format(method, accs))
+        print("Shares of    {}: {}".format(method, s_vec))
+        print("Costs  of    {}: {}".format(method, cost_of_clients))
+        print("Welfare of   {}: {}".format(method, [acc - cost for acc, cost in zip(accs, cost_of_clients)]))
